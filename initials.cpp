5be8f7ce7970710f249d3a4740ab9e8df4e1af1b
Dionuta <diounta@gmail.com>
2019-06-05 01:36:40 -0700

    Masterpiece

---
+++ initials.cpp
@@ -0,0 +1,15 @@
+// Fun Fact: I have really large big toes
+
+
+#include <iostream>
+
+int main() {
+
+  std::cout << "DDDD   BBBB\n";
+  std::cout << "D   D  B    B\n";
+  std::cout << "D   D  B    B\n";
+  std::cout << "D   D  BBBB\n";
+  std::cout << "D   D  B    B\n";
+  std::cout << "D   D  B    B\n";
+  std::cout << "DDDD   BBBB\n";
+}
